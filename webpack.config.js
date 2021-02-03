fc11a2017c3902086c600227a9a4da455b61dd63
Egor Panteleev <egor.panteleev@cashwagon.com>
2021-02-03 10:41:14 +0700

    fix/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
+    mode:"development",
     output: {
         path: path.resolve( __dirname, 'dist'),
         filename: 'bundle.js',
     },
 
     module:{
-        rules:[{
-            test: /\.(js|jsx)$/,
+        rules:[
+        {
+            test: /\.m?js$|jsx/,
             exclude: /node_module/,
-            loader: "babel-loader",
+            use: "babel-loader",
+           
+            
         },
-      
         {
             test: /\.css$/,
             use: 'css-loader'
         },
         {
             test: /\.(gif|svg|jpg|png)(\?v=\d+\.\d+\.\d+)?$/,
-            loader: "file-loader",
+            use: "file-loader",
             include: path.join(__dirname, 'public')
         },
         {
@@ -42,8 +44,15 @@ module.exports = {
     resolve: {
         extensions: [ '*', '.js', '.jsx']
     },
-    devtool: 'cheep-module-eval-source-map',
+    devtool: 'eval-cheap-module-source-map',
     devServer: {
-        conentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        inline: false,
+        compress: true
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
     }
 }
